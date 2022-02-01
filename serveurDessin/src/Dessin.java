dcca6e25862c3f6bf12d395ded9ac52e26443d10
LAOUARAugustin <55484260+augustin-laouar@users.noreply.github.com>
2022-02-01 09:21:22 +0100

    First

---
+++ serveurDessin/src/Dessin.java
@@ -0,0 +1,63 @@
+import java.awt.*;
+import java.awt.Color;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferStrategy;
+
+public class Dessin extends Frame {
+    private BufferStrategy strategie;
+    private Graphics2D graphics;
+    private int width;
+    private int height;
+    public Dessin(String msg, int w, int h) throws InterruptedException {
+        super(msg);
+        width = w;
+        height = h;
+        this.setBounds(100,0,width,height);
+        this.setVisible(true);
+        this.setIgnoreRepaint(true);
+        this.createBufferStrategy(2);
+        Thread.sleep(150);
+        strategie = this.getBufferStrategy();
+        graphics = (Graphics2D) strategie.getDrawGraphics();
+
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void afficher(){
+        strategie.show();
+    }
+    public void effacer(){
+        graphics.setColor(Color.white);
+        graphics.fillRect(0,0,width,height);
+    }
+    public void dessiner(Forme f){
+
+        ExpertCercleCOR e1 = new ExpertCercleCOR(null);
+        ExpertPolygoneCOR e2 = new ExpertPolygoneCOR(e1);
+        ExpertTraitCOR e3= new ExpertTraitCOR(e2);
+        e3.DessinerForme(f,graphics);
+
+
+
+    }
+
+}
---
+++ serveurDessin/src/TestDessin.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.text.Normalizer;
+
+public class TestDessin {
+    public static void main(String args[]){
+        try {
+            Dessin D = new Dessin("Test de dessin",1000,1000);
+            Ecouteur f = new Ecouteur();
+            D.addWindowListener(f);
+            Point p = new Point(410,460);
+        //  Cercle c = new Cercle(p,50);
+
+          //  D.afficher();
+            //test pour dessiner un trait
+           Point p2 = new Point(460,415);
+           Point p3 = new Point(500,425);
+           Point p4 = new Point(480,435);
+            Point p5 = new Point(550,445);
+            Point p6= new Point(410,460);
+            Forme f1 = new Forme();
+            f1.ListePoints.add(p);
+            f1.ListePoints.add(p2);
+            f1.ListePoints.add(p3);
+            f1.ListePoints.add(p4);
+            f1.ListePoints.add(p5);
+            f1.ListePoints.add(p6);
+
+            D.dessiner(f1);
+            D.afficher();
+        }
+
+        catch(InterruptedException e){
+            System.out.println("Erreur\n");
+        }
+
+    }
+}
